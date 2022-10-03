9ff540f02ce90e148fe34f24806d661385e8ebb8
Jonathan Grant <jonathangrant@Jonathans-MacBook-Air.local>
2022-10-03 16:04:41 -0400

    updated the function to add menu items and adding them up correctly

--- src/Menu.js
+++ src/Menu.js
@@ -7,7 +7,7 @@ const Menu = () => {
     {
       category: "food",
       food: "CHICKEN && WAFFLES",
-      price: "$11.99",
+      price: 12,
       img: "https://images.unsplash.com/photo-1565880112491-e7c4c313850d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80",
       description:
         " 3pc Southern Styled season chicken served with fresh fluffy waffles ",
@@ -16,7 +16,7 @@ const Menu = () => {
     {
       category: "food",
       food: "SHRIMP && GRITS",
-      price: "$12.99",
+      price: 13,
       img: "https://tmbidigitalassetsazure.blob.core.windows.net/rms3-prod/attachments/37/1200x1200/Cheesy-Cajun-Shrimp-and-Grits_EXPS_TOHDJ20_199562_B08_06_3b.jpg",
       description: "",
     },
--- src/Order.js
+++ src/Order.js
@@ -2,13 +2,18 @@ import React from "react";
 
 const Order = ({ order }) => {
   console.log("order", order);
+  let totalPrice = order.reduce(function (prev, current) {
+    return prev + +current.price;
+  }, 0);
+
   return (
     <div>
       {order.map((ord) => (
         <p>
-          [{ord.food}] {ord.price}
+          {ord.food} Price: ${ord.price}.00
         </p>
       ))}
+      <p> ${totalPrice}.00</p>
     </div>
   );
 };
