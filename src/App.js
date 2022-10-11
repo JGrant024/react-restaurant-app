65be1904425f7f6cc045ff52137dc8ed38cb899d
JGrant024 <jonathan.grant23@icloud.com>
2022-10-10 21:44:00 -0400

    updated background img and nav bar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import Menu from "./Menu";
 import "./App.css";
+import Navbar from "react-bootstrap/Navbar";
+import { Container } from "react-bootstrap";
 
 function App() {
   return (
     <div className="App">
-      <h2>Marvelous Meals & Brunch </h2>
+      <Navbar key={""} bg="light" expand={""} className="mb-3">
+        <Container>
+          {" "}
+          <Navbar.Brand href="#">Navbar Offcanvas</Navbar.Brand>
+        </Container>
+      </Navbar>
+      <h2>Marvelous Munchies</h2>
       <Menu />
     </div>
   );
